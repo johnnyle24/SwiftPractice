a50b19d29a601cb77b980736dad14359306d82b4
Johnny Le <Johnny@johnnys-macbook-pro.local>
2016-02-08 09:01:32 -0700

    Initial Commit

---
+++ Brush ChooserTests/Brush_ChooserTests.swift
@@ -0,0 +1,36 @@
+//
+//  Brush_ChooserTests.swift
+//  Brush ChooserTests
+//
+//  Created by Johnny Le on 2/8/16.
+//  Copyright © 2016 Johnny Le. All rights reserved.
+//
+
+import XCTest
+@testable import Brush_Chooser
+
+class Brush_ChooserTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
